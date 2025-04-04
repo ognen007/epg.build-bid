567e727229442eb02815605e8a86d4e33a6dfc01
ognen007 <ognenvl@gmail.com>
2025-04-04 23:26:30 +0200

    changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,15 +2,17 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDZySvF0SYvE4BdU57Of4P1_ekfOCnGvio",
-    authDomain: "epgghl.firebaseapp.com",
-    projectId: "epgghl",
-    storageBucket: "epgghl.appspot.com",
-    messagingSenderId: "1027773975545",
-    appId: "1:1027773975545:web:721e84a865e1d24874cb28",
-    measurementId: "G-V6N9RPV8S5"
-  };
-  
+  apiKey: "AIzaSyDZySvF0SYvE4BdU57Of4P1_ekfOCnGvio",
+  authDomain: "epgghl.firebaseapp.com",
+  projectId: "epgghl",
+  storageBucket: "epgghl.appspot.com",
+  messagingSenderId: "1027773975545",
+  appId: "1:1027773975545:web:721e84a865e1d24874cb28",
+  measurementId: "G-V6N9RPV8S5"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
@@ -19,7 +21,7 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/favicon.ico', // Optional: Change this to your app's icon
+    icon: '/favicon.ico', 
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);

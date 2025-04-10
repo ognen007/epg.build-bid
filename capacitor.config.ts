12991709794516faa02e5e50aff8c0f2adcba122
Ognen Veljanovski <127666100+ognen007@users.noreply.github.com>
2025-04-10 22:05:31 +0200

    chore: update dependencies to include Capacitor and Firebase plugins

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.epg.build',
+  appName: 'EPG',
+  webDir: 'dist'
+};
+
+export default config;
